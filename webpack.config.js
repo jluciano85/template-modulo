ffd9337b3ecb4a21fc40fd10ade471256ed8cba7
jluciano85 <jluciano85@yahoo.com.br>
2023-02-18 01:27:12 -0300

    adicionada pastas e arquivos

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader']          
+             }
+        ]
+    },
+
+    //watch: true,
+
+        devServer:{
+        static: path.resolve(__dirname, 'dist'),
+        open: true,
+        hot: true
+    }
+}
